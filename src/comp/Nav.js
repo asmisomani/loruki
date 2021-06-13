d2061b3705b46b997c8e90c0c92f111ae8e228ef
asmisomani <82661514+asmisomani@users.noreply.github.com>
2021-06-13 13:52:52 +0530

    changed route 2

--- src/comp/Nav.js
+++ src/comp/Nav.js
@@ -3,7 +3,7 @@ function Nav(){
     return <nav>
         <h2>Loruki.</h2>
         <ul>
-            <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
+            <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
             <li><NavLink to="/features" activeClassName="active">Features</NavLink></li>
             <li><NavLink to="/docs" activeClassName="active">Docs</NavLink></li>
         </ul>
